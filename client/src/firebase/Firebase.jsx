399fed85690a7724883d38bb1f40184c21244fcf
Tushit Palamkar <143091687+TushitPalamkar@users.noreply.github.com>
2026-01-07 12:33:28 +0530

    Update Firebase configuration and initialization

--- client/src/firebase/Firebase.jsx
+++ client/src/firebase/Firebase.jsx
@@ -1,17 +1,38 @@
 // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCgM17uuqqe3_4s6pS9X1fgvStQ6kB1f3Q",
+//   authDomain: "augenblick-eb72e.firebaseapp.com",
+//   projectId: "augenblick-eb72e",
+//   storageBucket: "augenblick-eb72e.firebasestorage.app",
+//   messagingSenderId: "662586563303",
+//   appId: "1:662586563303:web:c711157defe235ca186042"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCgM17uuqqe3_4s6pS9X1fgvStQ6kB1f3Q",
-  authDomain: "augenblick-eb72e.firebaseapp.com",
-  projectId: "augenblick-eb72e",
-  storageBucket: "augenblick-eb72e.firebasestorage.app",
-  messagingSenderId: "662586563303",
-  appId: "1:662586563303:web:c711157defe235ca186042"
+  apiKey: "AIzaSyCuPX_JY4ly7TaqkZJw93VbJkwB7oOlxK8",
+  authDomain: "augenblick-837ca.firebaseapp.com",
+  projectId: "augenblick-837ca",
+  storageBucket: "augenblick-837ca.firebasestorage.app",
+  messagingSenderId: "172977420910",
+  appId: "1:172977420910:web:0104fe54585329997e847f",
+  measurementId: "G-XN7BM9VYW4"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
