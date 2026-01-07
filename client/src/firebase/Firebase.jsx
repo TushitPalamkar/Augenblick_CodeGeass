66d09c65104238664aee985a4d2815102bdea88e
Tushit Palamkar <143091687+TushitPalamkar@users.noreply.github.com>
2026-01-07 12:40:10 +0530

    Update Firebase.jsx

--- client/src/firebase/Firebase.jsx
+++ client/src/firebase/Firebase.jsx
@@ -17,7 +17,7 @@
 // export const app = initializeApp(firebaseConfig);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -34,5 +34,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
